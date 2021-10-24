d3332baa893e6ca394618077ae50e8abe79949a1
KuliakQA <culeacvadim@gmail.com>
2021-10-24 19:05:57 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int accountBalance = 2_000_000_000;
+        int translation = 500_000_000;
+        int total = accountBalance + translation;
+        System.out.println(total);
+
+    }
+}
